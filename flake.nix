561f59e08c36ff98d93cce6922e67d75cc2db027
Vladimir Serov <me@cab404.ru>
2021-04-22 13:59:45 +0300

    added nixos module

--- flake.nix
+++ flake.nix
@@ -27,6 +27,42 @@
           RUST_SRC_PATH = rustPlatform.rustLibSrc;
         };
 
+        nixosModule = { system, config, ... }:
+        let name = "undefspace-tg-bot";
+        in with nixpkgs.lib; {
+            options = {
+                services.${name} = {
+                    enable = mkEnableOption "enables undefspace telegram bot";
+                    config = mkOption {
+                        type = types.path;
+                        default = null;
+                        description = ''
+                          path to environment config
+                        '';
+                        example = ''
+                          writeTextFile '''
+                            TELOXIDE_TOKEN="telegram token"
+                            HASS_TOKEN="home assistant token"
+                            CONTROL_CHAT_ID="id of a control chat"
+                          ''';
+                        '';
+                    };
+                };
+            };
+            config = mkIf config.services.${name}.enable {
+                systemd.services.${name} = {
+                    script = ''
+                      source $CONFIG
+                      ${self.defaultPackage."${system}"}/bin/${name}
+                    '';
+                    environment = {
+                        CONFIG = config.services.${name}.config;
+                    };
+                };
+
+            };
+        };
+
       });
 
 }
