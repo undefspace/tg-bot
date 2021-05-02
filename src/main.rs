d4f201547cf891f6f455ca2a0f042ab52bfbac9c
Vladimir Serov <me@cab404.ru>
2021-05-02 16:23:36 +0300

    looping over listener, so it restarts on errors

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,12 @@
-use futures::{StreamExt};
+use futures::StreamExt;
 use log::{debug, warn};
 use reqwest;
 use serde::{Deserialize, Serialize};
 use std::{collections::HashMap, env, sync::Arc, time::Duration, usize};
 use teloxide::{
     dispatching::update_listeners,
-    prelude::{
-        GetChatId, Request,
-        UpdateWithCx,
-    },
-    types::{
-        MessageEntityKind, UpdateKind,
-    },
+    prelude::{GetChatId, Request, UpdateWithCx},
+    types::{MessageEntityKind, UpdateKind},
 };
 use tokio::sync::mpsc::channel;
 use tokio::{sync::Mutex, time::sleep};
@@ -75,83 +70,84 @@ async fn run() {
         });
         tx
     }));
-    // let boxed = Box::pin(door_opener);
+
     let bot = teloxide::Bot::from_env();
 
-    let mut f = Box::pin(update_listeners::polling_default(bot.clone()));
-    while let Some(update) = f.next().await {
-        debug!("A new update arrived {:?}", update);
-        let update = match update {
-            Ok(update) => update,
-            Err(error) => {
-                // Arc::clone(&update_listener_error_handler).handle_error(error).await;
-                warn!("Error while receiving update: {}", error);
-                return;
-            }
-        };
-        let result: Result<(), &str> = async {
-            match update.kind {
-                UpdateKind::Message(msg) => {
-                    let cx = UpdateWithCx {
-                        update: msg,
-                        requester: bot.clone(),
-                    };
-
-                    if cx.chat_id() != control_chat_id {
-                        Err("Message outside the control chat.")?;
-                    }
-                    let text = cx
-                        .update
-                        .text()
-                        .ok_or("no message in a text message :/")?
-                        .to_string();
-                    let commands = cx
-                        .update
-                        .entities()
-                        .ok_or("no entities")?
-                        .iter()
-                        .filter(|c| c.kind == MessageEntityKind::BotCommand)
-                        .map(|s| text[s.offset + 1..s.offset + s.length].to_string())
-                        .filter(|s| s.ends_with("@undefspace_bot"))
-                        .map(|s| s.trim_end_matches("@undefspace_bot").to_string());
-
-                    for command in commands {
-                        let res = match command.as_str() {
-                            "open" => {
-                                let sender = cx.update.from().ok_or("No sender: no sending")?;
-                                &cx.reply_to(format!(
-                                    "Opening a door for *{} [@{}]*",
-                                    &sender.full_name(),
-                                    &sender.username.clone().unwrap_or("".to_string())
-                                ))
-                                .send()
-                                .await;
-                                door_opener
-                                    .lock()
-                                    .await
-                                    .try_send(())
-                                    .map_err(|e| e.to_string())
-                            }
-                            unk => Err(unk.to_string()),
+    loop {
+        let mut f = Box::pin(update_listeners::polling_default(bot.clone()));
+        while let Some(update) = f.next().await {
+            debug!("A new update arrived {:?}", update);
+            let update = match update {
+                Ok(update) => update,
+                Err(error) => {
+                    warn!("Error while receiving update: {}", error);
+                    return;
+                }
+            };
+            let result: Result<(), &str> = async {
+                match update.kind {
+                    UpdateKind::Message(msg) => {
+                        let cx = UpdateWithCx {
+                            update: msg,
+                            requester: bot.clone(),
                         };
-                        if let Err(command) = res {
-                            tokio::spawn(
-                                cx.reply_to(format!("Cannot process command '{}'!", command))
-                                    .send(),
-                            );
-                            warn!("Cannot process command '{}'!", command)
+
+                        if cx.chat_id() != control_chat_id {
+                            Err("Message outside the control chat.")?;
+                        }
+                        let text = cx
+                            .update
+                            .text()
+                            .ok_or("no message in a text message :/")?
+                            .to_string();
+                        let commands = cx
+                            .update
+                            .entities()
+                            .ok_or("no entities")?
+                            .iter()
+                            .filter(|c| c.kind == MessageEntityKind::BotCommand)
+                            .map(|s| text[s.offset + 1..s.offset + s.length].to_string())
+                            .filter(|s| s.ends_with("@undefspace_bot"))
+                            .map(|s| s.trim_end_matches("@undefspace_bot").to_string());
+
+                        for command in commands {
+                            let res = match command.as_str() {
+                                "open" => {
+                                    let sender = cx.update.from().ok_or("No sender: no sending")?;
+                                    &cx.reply_to(format!(
+                                        "Opening a door for *{} [@{}]*",
+                                        &sender.full_name(),
+                                        &sender.username.clone().unwrap_or("".to_string())
+                                    ))
+                                    .send()
+                                    .await;
+                                    door_opener
+                                        .lock()
+                                        .await
+                                        .try_send(())
+                                        .map_err(|e| e.to_string())
+                                }
+                                unk => Err(unk.to_string()),
+                            };
+                            if let Err(command) = res {
+                                tokio::spawn(
+                                    cx.reply_to(format!("Cannot process command '{}'!", command))
+                                        .send(),
+                                );
+                                warn!("Cannot process command '{}'!", command)
+                            }
                         }
                     }
+                    _ => {}
                 }
-                _ => {}
+                Ok(())
             }
-            Ok(())
-        }
-        .await;
+            .await;
 
-        match result {
-            Ok(_) => {}
-            Err(e) => debug!("Error while handling an update: {}", e),
-        };
+            match result {
+                Ok(_) => {}
+                Err(e) => debug!("Error while handling an update: {}", e),
+            };
+        }
     }
 }
